5fb526257638decd6059f1fa907b9933b5fa0d29
autumn <102363146+autumnlikescode@users.noreply.github.com>
2024-04-12 15:55:02 +1000

    Update rewasd_detector.cpp

--- rewasd_detector.cpp
+++ rewasd_detector.cpp
@@ -16,14 +16,14 @@ INLINE bool IsDriverLoaded(const char* driverName) {
             char driverPath[MAX_PATH];
             if (GetDeviceDriverBaseNameA(drivers[i], driverPath, sizeof(driverPath))) {
                 if (strcmp(driverPath, driverName) == 0) {
-                    std::cout << OBF("Driver found: ") << driverName << std::endl;
+                    std::cout << "Driver found: " << driverName << std::endl;
                     
 
                     if (SimulateGame)
                         MessageBox(
                             NULL,
-                            (LPCWSTR)OBF(L"Not-Vanguard has detected the following incompatible software on your system: \n\n hidgamemap.sys \n\n\n To play Not-Vanguard protet games you will need to remove any incompatible software. \n\n Would you like to learn more?"),
-                            (LPCWSTR)OBF(L"Not-Riot Not-Vanguard"),
+                            (LPCWSTR)L"Not-Vanguard has detected the following incompatible software on your system: \n\n hidgamemap.sys \n\n\n To play Not-Vanguard protet games you will need to remove any incompatible software. \n\n Would you like to learn more?",
+                            (LPCWSTR)L"Not-Riot Not-Vanguard",
                             MB_ICONWARNING | MB_YESNO
                         );
 
@@ -33,12 +33,12 @@ INLINE bool IsDriverLoaded(const char* driverName) {
         }
     }
     else {
-        std::cerr << OBF("Error: EnumDeviceDrivers failed") << std::endl;
+        std::cerr << "Error: EnumDeviceDrivers failed" << std::endl;
     }
 
 
 
-    std::cout << OBF("Driver not found: ") << driverName << std::endl;
+    std::cout << "Driver not found: " << driverName << std::endl;
     return false;
 }
 
@@ -46,13 +46,13 @@ void mainCode()
 {
     const char* targetDriver = "hidgamemap.sys";
     if (IsDriverLoaded(targetDriver)) {
-        std::cout << OBF("The driver ") << targetDriver << OBF(" is loaded.") << std::endl;
+        std::cout << "The driver " << targetDriver << " is loaded." << std::endl;
     }
     else {
-        std::cout << OBF("The driver ") << targetDriver << OBF(" is not loaded.") << std::endl;
+        std::cout << "The driver " << targetDriver << " is not loaded." << std::endl;
     }
 
-    system(OBF("PAUSE"));
+    system("PAUSE");
 }
 int main() {
     mainCode();
